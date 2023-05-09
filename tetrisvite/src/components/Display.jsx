73ca5e7c3ec2316bef294c2cced6e59385a9ff31
MatiasSoto21 <matiassoto00@outlook.com>
2023-05-08 23:36:20 -0300

    respo

--- tetrisvite/src/components/Display.jsx
+++ tetrisvite/src/components/Display.jsx
@@ -5,8 +5,8 @@ const Display = ({ gameOver, text }) => (
   <div
     className={
       gameOver
-        ? "text-red-500 text-xs md:text-xl lg:text-2xl box-border flex items-center p-[20px] border-4 mb-4 rounded-[20px] border-gray-500 lg:min-h-[30px] w-full bg-black font-[Pixel]"
-        : "text-white text-xs md:text-xl lg:text-2xl box-border flex items-center p-[20px] border-4 mb-4 rounded-[20px] border-gray-500 lg:min-h-[30px] w-full bg-black font-[Pixel]"
+        ? "text-red-500 text-xs md:text-xl lg:text-2xl box-border flex items-center p-[20px] border-4 mb-4 rounded-[20px] border-gray-500 lg:min-h-[30px] w-32 md:w-full lg:w-full bg-black font-[Pixel]"
+        : "text-white text-xs md:text-xl lg:text-2xl box-border flex items-center p-[20px] border-4 mb-4 rounded-[20px] border-gray-500 lg:min-h-[30px] w-32 md:w-full lg:w-full bg-black font-[Pixel]"
     }
   >
     {text}
